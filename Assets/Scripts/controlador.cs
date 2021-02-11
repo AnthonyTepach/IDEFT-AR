8fb325af563ddc1b659747c3f7fb17b85a6feec9
Programador Computer Forms <developer@Cf.LAN>
2021-02-11 11:21:24 -0600

    Initial commit

---
+++ Assets/Scripts/controlador.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using Firebase.Auth;
+
+public class controlador : MonoBehaviour
+{
+    public static controlador instancia;
+   
+   
+    private void Awake()
+    {
+        if (instancia == null) instancia = this;
+        else if (instancia != this) Destroy(gameObject);
+        DontDestroyOnLoad(this);
+       
+    }
+
+
+    FirebaseAuth auth = FirebaseAuth.DefaultInstance;
+
+
+    public InputField correo;
+    public Text aleta=null;
+
+    
+
+    public void nextScene(string nameScene){
+       SceneManager.LoadScene(nameScene);
+    }
+
+  
+
+   
+    public void login(string email,string password) {
+
+        password pws = new password();
+
+
+        auth.SignInWithEmailAndPasswordAsync(email, password).ContinueWith(task => {
+            if (task.IsCanceled)
+            {
+                Debug.LogError("SignInWithEmailAndPasswordAsync was canceled.");
+                return;
+            }
+            if (task.IsFaulted)
+            {
+                Debug.LogError("SignInWithEmailAndPasswordAsync encountered an error: " + task.Exception);
+                return;
+            }
+
+            Firebase.Auth.FirebaseUser newUser = task.Result;
+            Debug.LogFormat("User signed in successfully: {0} ({1})",
+                newUser.DisplayName, newUser.UserId);
+        });
+
+    }
+
+
+
+}
---
+++ Assets/Scripts/password.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class password : MonoBehaviour
+{
+
+    private string passwordToEdit = string.Empty;
+   
+
+    void OnGUI()
+    {//menos es izquierda,menos es arriba,,
+        passwordToEdit = GUI.PasswordField(new Rect(351f, 241f, 178f, 28f), passwordToEdit, "*"[0], 25);
+
+    }
+
+    public  string getPassword() {
+        return passwordToEdit;
+    }
+}
